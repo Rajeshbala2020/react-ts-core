6f4f2563715cfd4618370c8cc019ce0f6fb9ea79
Rajeshbala2020 <rajeshbala.qbs@gmail.com>
2023-10-09 09:35:52 +0530

    example updates

--- docs/App.tsx
+++ docs/App.tsx
@@ -20,97 +20,111 @@ export default function App() {
   };
   return (
     <React.Fragment>
-      <div style={{ display: 'block', gap: 20, padding: 20 }}>
-        <div className="autocomplete-section">
-          <h2>Custom Select</h2>
-          <p>
-            Select an option from a predefined static list. Ideal for a scenario
-            when the available options are limited and known.
-          </p>
-        </div>
-        <div style={{ width: 200 }}>
-          <AutoComplete
-            label="Custom Select"
-            name="sample"
-            type="custom_select"
-            data={[
-              { name: 'test', id: '1' },
-              { name: 'test1', id: '2' },
+      <div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 20,
+            padding: 20,
+            maxWidth: 700,
+          }}
+        >
+          <div className="autocomplete-section">
+            <h2>Custom Select</h2>
+            <p>
+              Select an option from a predefined static list. Ideal for a
+              scenario when the available options are limited and known.
+            </p>
+          </div>
+          <div style={{ width: 200 }}>
+            <AutoComplete
+              label="Custom Select"
+              name="sample"
+              placeholder="Custom Select"
+              type="custom_select"
+              data={[
+                { name: 'test', id: '1' },
+                { name: 'test1', id: '2' },
 
-              { name: 'test2', id: '3' },
+                { name: 'test2', id: '3' },
 
-              { name: 'test3', id: '4' },
-            ]}
-            getData={getData}
-            onChange={() => console.log('onchange')}
-          />
-        </div>
-        <div className="autocomplete-section">
-          <h2>Auto Complete</h2>
-          <p>
-            Type in the input field and get real-time suggestions from
-            dynamically fetched data. Useful for selecting from large datasets
-            where displaying all available options is impractical.
-          </p>
-        </div>
-        <div style={{ width: 200 }}>
-          <AutoComplete
-            label="Auto Complete"
-            name="sample"
-            type="auto_complete"
-            // data={[{ name: 'test', id: '1' }]}
-            getData={getData}
-            onChange={() => console.log('onchange')}
-          />
-        </div>
-        <div className="autocomplete-section">
-          <h2>Custom Search Select</h2>
-          <p>
-            This allows you to search through static data, providing a filtered
-            list based on your query or select from available items. Optimal for
-            scenarios where you might have predefined options but also want to
-            enable users to quickly narrow down their choices.
-          </p>
-        </div>
+                { name: 'test3', id: '4' },
+              ]}
+              getData={getData}
+              onChange={() => console.log('onchange')}
+            />
+          </div>
+          <div className="autocomplete-section">
+            <h2>Auto Complete</h2>
+            <p>
+              Type in the input field and get real-time suggestions from
+              dynamically fetched data. Useful for selecting from large datasets
+              where displaying all available options is impractical.
+            </p>
+          </div>
+          <div style={{ width: 200 }}>
+            <AutoComplete
+              label="Auto Complete"
+              name="sample"
+              type="auto_complete"
+              placeholder="Auto Complete"
+              // data={[{ name: 'test', id: '1' }]}
+              getData={getData}
+              onChange={() => console.log('onchange')}
+            />
+          </div>
+          <div className="autocomplete-section">
+            <h2>Custom Search Select</h2>
+            <p>
+              This allows you to search through static data, providing a
+              filtered list based on your query or select from available items.
+              Optimal for scenarios where you might have predefined options but
+              also want to enable users to quickly narrow down their choices.
+            </p>
+          </div>
 
-        <div style={{ width: 200 }}>
-          <AutoComplete
-            label="Custom Search Select"
-            name="sample"
-            type="custom_search_select"
-            data={[
-              { name: 'Apple', id: '1' },
-              { name: 'Banana', id: '2' },
-              { name: 'Cherry', id: '3' },
-              { name: 'Date', id: '4' },
-              { name: 'Fig', id: '5' },
-              { name: 'Grape', id: '6' },
-              { name: 'Honeydew', id: '7' },
-              { name: 'Kiwi', id: '8' },
-              { name: 'Lemon', id: '9' },
-              { name: 'Mango', id: '10' },
-            ]}
-            onChange={() => console.log('onchange')}
-          />
-        </div>
-        <div className="autocomplete-section">
-          <h2>Auto Suggestion</h2>
-          <p>
-            As you type, suggestions will appear based on your input, aiding in
-            quicker and potentially more accurate data entry. This mode supports
-            both listed and non-listed input, providing flexibility in user
-            input while still offering assistance.
-          </p>
-        </div>
-        <div style={{ width: 200 }}>
-          <AutoComplete
-            label="Auto Suggestion"
-            name="sample"
-            type="auto_suggestion"
-            // data={[{ name: 'test', id: '1' }]}
-            getData={getData}
-            onChange={() => console.log('onchange')}
-          />
+          <div style={{ width: 200 }}>
+            <AutoComplete
+              label="Custom Search Select"
+              name="sample"
+              placeholder="Custom Search Select"
+              type="custom_search_select"
+              data={[
+                { name: 'Apple', id: '1' },
+                { name: 'Banana', id: '2' },
+                { name: 'Cherry', id: '3' },
+                { name: 'Date', id: '4' },
+                { name: 'Fig', id: '5' },
+                { name: 'Grape', id: '6' },
+                { name: 'Honeydew', id: '7' },
+                { name: 'Kiwi', id: '8' },
+                { name: 'Lemon', id: '9' },
+                { name: 'Mango', id: '10' },
+              ]}
+              onChange={() => console.log('onchange')}
+            />
+          </div>
+          <div className="autocomplete-section">
+            <h2>Auto Suggestion</h2>
+            <p>
+              As you type, suggestions will appear based on your input, aiding
+              in quicker and potentially more accurate data entry. This mode
+              supports both listed and non-listed input, providing flexibility
+              in user input while still offering assistance.
+            </p>
+          </div>
+          <div style={{ width: 200 }}>
+            <AutoComplete
+              label="Auto Suggestion"
+              name="sample"
+              type="auto_suggestion"
+              placeholder="Auto Suggestion"
+              // data={[{ name: 'test', id: '1' }]}
+              getData={getData}
+              onChange={() => console.log('onchange')}
+            />
+          </div>
         </div>
       </div>
     </React.Fragment>
