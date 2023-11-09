877302f4ca3524df811138266e88a76b7a98dc5d
Rajagopal B <rajagopalb94@gmail.com>
2023-11-09 17:51:50 +0530

    add autocomplete dropdown UI and updates

--- src/AutoComplete.tsx
+++ src/AutoComplete.tsx
@@ -207,7 +207,7 @@ const AutoComplete: FC<AutoSuggestionInputProps> = ({
 
               {selectedItems.length > 1 && (
                 <div className="selected-item-more">
-                  +{selectedItems.length - 1} more
+                  +{selectedItems.length - 1}
                 </div>
               )}
             </>
@@ -292,15 +292,33 @@ const AutoComplete: FC<AutoSuggestionInputProps> = ({
                 filteredData.map((suggestion: ValueProps, idx: number) => (
                   <div
                     key={idx.toString()}
-                    className="qbs-autocomplete-listitem-container"
+                    className={`qbs-autocomplete-listitem-container ${
+                      (isMultiple || singleSelect) &&
+                      "qbs-autocomplete-checkbox-container"
+                    } ${isSelected(suggestion, selected) ? "is-selected" : ""}`}
                   >
                     {(isMultiple || singleSelect) && (
-                      <div>
+                      <div className="qbs-autocomplete-checkbox">
                         <input
                           onChange={(e) => handleMultiSelect(e, suggestion)}
                           type="checkbox"
                           checked={isSelected(suggestion, selected)}
+                          id={`qbs-checkbox-${idx.toString()}`}
                         />
+                        <label htmlFor={`qbs-checkbox-${idx.toString()}`}>
+                          <svg
+                            width="8"
+                            height="6"
+                            viewBox="0 0 8 6"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M0 3.21739L2.89883 6L8 1.06994L6.89494 0L2.89883 3.86768L1.09728 2.14745L0 3.21739Z"
+                              fill="white"
+                            />
+                          </svg>
+                        </label>
                       </div>
                     )}
                     <li
