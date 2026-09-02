f2a98b91600c0890dcc653a4f113dc51f20e27c2
Rajeshbala2020 <rajeshbala.qbs@gmail.com>
2026-09-02 13:42:53 +0530

    comit verdion

--- src/utilities/debounce.ts
+++ src/utilities/debounce.ts
@@ -2,7 +2,7 @@ export function debounce<F extends (...args: any[]) => any>(
   fn: F,
   delay: number
 ): (...args: Parameters<F>) => void {
-  let timerId: NodeJS.Timeout | null = null;
+  let timerId: ReturnType<typeof setTimeout> | null = null;
   return (...args: Parameters<F>): void => {
     if (timerId) {
       clearTimeout(timerId);
